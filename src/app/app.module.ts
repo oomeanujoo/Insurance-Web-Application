8e7f5a42925f1be05ba4ef05f2e791767e5ee059
oomeanujoo <anuzz8602@gmail.com>
2023-08-03 16:48:41 +0530

    ng bootstrap removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { ToastrModule, ToastNoAnimationModule, ToastContainerModule, } from 'ngx-toastr';
-import { NgxOtpInputModule } from 'ngx-otp-input'; 
+import { NgxOtpInputModule } from 'ngx-otp-input';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DatePickerDirective } from './date-picker.directive';
@@ -27,7 +27,7 @@ import { DatePickerDirective } from './date-picker.directive';
     NgxOtpInputModule,
     NgbModule,
     FormsModule, ReactiveFormsModule
-  
+
   ],
   providers: [],
   bootstrap: [AppComponent]
